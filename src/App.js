fe26cf4e99de08b9bc3c879bf5c5ef56e1bc340e
Dominique <dominique@angel.co>
2023-02-27 16:03:59 -0800

    first public release

--- src/App.js
+++ src/App.js
@@ -1,30 +1,121 @@
 import logo from "./logo.svg";
 import "./App.css";
 import styles from "./index.css";
-import img from "./videos/splash.jpg";
 import vid from "./videos/bg.mp4";
+import NavItem from "./NavItem";
+import React, { useRef, useState } from "react";
 
 function App() {
+  const musicRef = useRef();
+  const conceptRef = useRef();
+  const synopsisRef = useRef();
+
+  const [hoveredLink, setHoveredLink] = useState("");
+
   return (
     <div className="App">
-      <nav className="text-white">
-        <a href="#">Bio</a>
-        <a href="#">Resume</a>
-        <a href="#">Media</a>
+      <nav
+        className={`w-screen h-10 md:hover:h-10 flex fixed top-0 z-30  transition-[height] duration-200 overflow-hidden`}
+      >
+        <NavItem
+          onClick={() =>
+            synopsisRef.current.scrollIntoView({ behavior: "smooth" })
+          }
+          bgColor="hover:border-blue-600"
+          onHover={setHoveredLink}
+          hoveredLink={hoveredLink}
+          label="bio"
+        />
+        <NavItem
+          onClick={() =>
+            musicRef.current.scrollIntoView({ behavior: "smooth" })
+          }
+          onHover={setHoveredLink}
+          bgColor="hover:border-pink-600"
+          hoveredLink={hoveredLink}
+          label="resume"
+        />
+        <NavItem
+          onClick={() =>
+            conceptRef.current.scrollIntoView({ behavior: "smooth" })
+          }
+          bgColor="hover:border-green-600"
+          onHover={setHoveredLink}
+          hoveredLink={hoveredLink}
+          label="media"
+        />
+        <NavItem
+          onClick={() =>
+            conceptRef.current.scrollIntoView({ behavior: "smooth" })
+          }
+          bgColor="hover:border-green-600"
+          onHover={setHoveredLink}
+          hoveredLink={hoveredLink}
+          label="gallery"
+        />
       </nav>
-      <div className="w-200 h-200 fixed">
+      <div className="w-200 h-200 fixed -z-10">
         {/* <img
           src={img}
           className="max-h-100 max-w-100"
           alt="dominique star website background"
         /> */}
-        <video id="background-video" loop autoPlay muted>
+        <video id="background-video" loop autoPlay muted className="fixed">
           <source src={vid} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
-      <div>
-        <h1>Dominique Star</h1>
+      <div className="w-screen h-screen m-auto text-center uppercase text-white">
+        <div className="relative top-1/3 ">
+          <h1>Dominique Star</h1>
+
+          <h2>Actor, Singer, Musician</h2>
+        </div>
+      </div>
+      <div className="relative h-screen w-screen bg-black/95" ref={synopsisRef}>
+        <div className="text-white text-left m-auto max-w-[600px] relative top-1/3 bg-black p-4">
+          <p>
+            Dominique Star is an actor, singer, and multi-instrumentalist from
+            Rhode Island. She attended Columbia University and spent several
+            years as a musician in NYC before moving to LA. She's drawn to
+            free-spirited, sarcastic, edgy, artsy characters.
+          </p>
+
+          <p className="mt-4">
+            As a musician, she's sung on stage with MIKA, has played guitar and
+            piano since the age of 14, and has had her music featured in network
+            and streaming series across the globe.
+          </p>
+
+          <p className="mt-4">
+            As an actor, her work on Jay Shetty's webseries has been viewed
+            millions of times across social media, and her voiceover career has
+            spanned from national commercials to award winning video games such
+            as Yi and the Thousand Moons.
+          </p>
+        </div>
+      </div>
+      <div
+        className="h-screen w-screen bg-black/95 pt-10 text-white"
+        ref={musicRef}
+      >
+        <iframe
+          className="m-auto"
+          src="https://drive.google.com/file/d/1gLynsLkuVIZpZWK2QtXW2fcmWoaD9qtE/preview"
+          width="640"
+          height="800"
+          allow="autoplay"
+          title="Dominique Star's Acting Resume"
+        ></iframe>
+        <a
+          className="p-2 bg-purple-600 border border-white"
+          href="https://drive.google.com/uc?export=download&id=1gLynsLkuVIZpZWK2QtXW2fcmWoaD9qtE"
+        >
+          Download Resume
+        </a>
+      </div>
+      <div className="h-screen w-screen bg-black/95" ref={conceptRef}>
+        <div className="relative top-1/3 text-white">Coming Soon :)</div>
       </div>
     </div>
   );
---
+++ src/NavItem.js
@@ -0,0 +1,14 @@
+export default function NavItem(props) {
+  const { onClick, bgColor, label, onHover } = props;
+
+  return (
+    <button
+      onClick={() => onClick()}
+      onMouseEnter={() => onHover(label)}
+      onMouseLeave={() => onHover("")}
+      className={`${bgColor} border-b w-full uppercase bg-black/90 pl-4 h-full duration-200 font-sans text-white text-right pr-4 `}
+    >
+      {label}
+    </button>
+  );
+}
