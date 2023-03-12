3b4a07fe2c2a6f0b540c2dea42fb9caf2b4454cc
Dominique <dominique@angel.co>
2023-03-11 16:24:53 -0800

    primary headshot

--- src/App.js
+++ src/App.js
@@ -211,7 +211,7 @@ function App() {
           </div>
           <div>
             <img
-              src="DominiqueStarHeadshot1.jpeg?v=5"
+              src="DominiqueStarHeadshot1.jpeg?v=6"
               alt="Dominique Star Headshot"
             />
           </div>
