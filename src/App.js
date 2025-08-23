5a9cb8845865d97bfbc85ff80dab6bdd780ab5a5
Dominique <dominique@angel.co>
2025-08-23 12:17:47 -0700

    update headshots

--- src/App.js
+++ src/App.js
@@ -167,7 +167,7 @@ function App() {
 export function Home(){
   return (
     <div className="md:px-8 mt-4 space-y-6 md:w-3/4 m-auto text-left">
-      DOMINIQUE STAR is a fearless multi-hyphenate who splits her time between Los Angeles and London. She was most recently seen on screen in <i>Ryan Murphy's "Doctor Odyssey"</i>, and on stage as <i>Ophelia</i> in a groundbreaking <i>Hamlet x Radiohead </i> musical, Perchance to Dream.
+      DOMINIQUE STAR is an actor & musician who splits her time between Los Angeles and London. She was most recently seen on screen in <i>Ryan Murphy's "Doctor Odyssey"</i>, and on stage as <i>Ophelia</i> in a groundbreaking <i>Hamlet x Radiohead </i> musical, Perchance to Dream.
             <iframe
               className="youtube-video"
               src="https://www.youtube.com/embed/AaLxC87yG9E"
@@ -183,7 +183,7 @@ export function Home(){
               alt="Dominique Star Headshot"
             />
             <img className=""
-              src="/modelling/6.jpeg"
+              src="DominiqueStarHeadshot1.jpg"
               alt="Dominique Star Headshot"
             />
 
